9a136536b75a46d08074142409f642c9b1eded76
appleater <appleater7@gmail.com>
2019-07-16 11:26:35 +0900

    ws_timer

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.listen(82,function(){
 var websocket = new ws.Server({port:808})
 websocket.on('connection', function(socket){
     console.log('ws connected!');
+    // WebSocket 에 메시지 이벤트를 발생시킴
+    setInterval(function(){
+        socket.send(new Date().toString());
+    },1000)    
 });
 // websocket.on('listening', function(){
 
